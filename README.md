b380f28ce4307df3d51f5763117f69c8fa6778ca
류윤광/회원채널개발팀 <yungwang.ryu@musina.com>
2021-08-26 13:33:34 +0900

    스프링 어드민 환경 구성

---
+++ README.md
@@ -0,0 +1,12 @@
+## 멀티모듈 환경 기술 연습 프로젝트
+하나하나 살을 붙여가 보자
+
+## 모듈구성
+### biggwang-monitor
+- spring admin 으로 인스턴스 상태 확인
+### biggwang-reactive
+- webflux + reactive mongodb 구성
+
+## TODO
+- 로그 쉽게 볼수 있도록 키바나 구성해보자
+- aws 환경에 올려놓고 부하테스트 하며 동작원리 파악하자
--- biggwang-monitor/src/main/java/com/biggwang/monitor/MonitorApplication.java
+++ biggwang-monitor/src/main/java/com/biggwang/monitor/MonitorApplication.java
@@ -1,11 +1,13 @@
 package com.biggwang.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 
+@EnableAdminServer
 @SpringBootApplication
 public class MonitorApplication {
 
